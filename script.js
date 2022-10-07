2ff6e2fd57779403c13a81468971a9dcc67a6422
Agustinmiguel04 <agusmiguel71@gmail.com>
2022-10-06 23:12:41 -0300

    se agrega el tp

---
+++ script.js
@@ -0,0 +1,11 @@
+//#region 
+function sumatoria (n1,n2){
+    var sumatoria = n1 + n2 
+    return sumatoria;
+}
+var num1=4;
+var num2=6;
+let suma = sumatoria(num1,num2); 
+console.log(suma);
+//#endregion 
+|
