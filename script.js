2e664162ecb533984e10e29142b478329db3b698
Agustinmiguel04 <agusmiguel71@gmail.com>
2022-10-06 23:36:09 -0300

    se agrega divisin

---
+++ script.js
@@ -0,0 +1,30 @@
+//#region 
+function sumatoria (n1,n2){
+    var sumatoria = n1 + n2 
+    return sumatoria;
+}
+var num1=4;
+var num2=6;
+let suma = sumatoria(num1,num2); 
+console.log(suma);
+//#endregion 
+//#region 
+function restar(n3,n4){
+    var resta = n3 - n4;
+    return resta;
+}
+var num3=15;
+var num4=10;
+let resta= restar(num3,num4);
+console.log(resta);
+//#endregion
+//#region
+function division(n5,num6){
+    var division = n5/n6;
+    return division;
+}
+var numero5=12;
+var numero6=4;
+let divi = division(numero5,numero6);
+console.log(divi);
+//#endregion
